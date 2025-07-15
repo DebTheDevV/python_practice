7f35678107e41615b055614ed5263e94e4cb323e
DebTheDevV <debthedevv@gmail.com>
2025-07-15 16:57:45 +0530

    into and lec1 learned

---
+++ lec1.py
@@ -0,0 +1,6 @@
+# learning modules and pip
+import pandas #external module
+import hashlib #built in lib
+# import tensorflow , yaha error aa jayega as tensor flow is not downloaded
+print("hi")
+pandas.read_csv('words.csv')
